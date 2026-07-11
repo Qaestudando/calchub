5d4d2fb0ab0c5397de3a78a26f7f2c0f5e81da5f
qaestudando <qaestudando@gmail.com>
2026-07-11 14:08:38 -0300

    front

--- frontend/app/calculators/compound-interest/page.tsx
+++ frontend/app/calculators/compound-interest/page.tsx
@@ -1,20 +1,5 @@
+import CompoundInterestCalculator from "@/src/components/calculators/compound-interest/Component";
 
-import Header from "@/src/components/layout/Header";
-import Hero from "@/src/components/home/Hero";
-import Footer from "@/src/components/layout/Footer";
-import Container from "@/src/components/layout/Container";
-import CategoryGrid from "@/src/components/home/CategoryGrid";
-import PopularCalculators from "@/src/components/home/PopularCalculators";
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <>
-      <Header />
-
-      <Hero />
-
-      <Footer />
-    </>
-  );
+export default function CompoundInterestPage() {
+  return <CompoundInterestCalculator />;
 }
